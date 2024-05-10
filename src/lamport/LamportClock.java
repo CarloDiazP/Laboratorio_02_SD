de6b4d9b8ddbe0b5283858febdc43dcaf72ca628
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-05-10 09:36:22 -0500

    Agregamos comentarios que ayuden a comprender el código

--- src/lamport/LamportClock.java
+++ src/lamport/LamportClock.java
@@ -8,50 +8,63 @@ public class LamportClock {
     private int clock;
 
     public LamportClock() {
-        this.clock = 0;
+        this.clock = 0; // Estado inicial del reloj
     }
 
     public synchronized int tick() {
-        this.clock++;
+        this.clock++; // Aumenta en uno el reloj y lo retorna
         return this.clock;
     }
 
     public synchronized void update(int receivedTime) {
+        // Actualiza el tiempo del reloj con el máximo entre el tiempo
+        // actual y el tiempo recibido como argumento más 1.
         this.clock = Math.max(this.clock, receivedTime) + 1;
     }
 
     public int getTime() {
+        // Retorna el valor del reloj actual
         return this.clock;
     }
 
     public static void main(String[] args) {
+        // Se crea una lista de hilos para simular concurrencia
         List<Thread> threads = new ArrayList<>();
+        // Se instancia el reloj
         LamportClock clock = new LamportClock();
+        // Se crearán 5 hilos
         for (int i = 0; i < 5; i++) {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    // Obtenemos el tiempo actual
                     int time = clock.tick();
 
                     System.out.println(
                             "Thread " + Thread.currentThread().getId() + " created event with Lamport time " + time);
                     try {
+                        // Simulamos un tiempo de espera aleatorio
                         Thread.sleep((long) (Math.random() * 1000));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
+                    // Obtenemos el tiempo actualizado
                     int receivedTime = clock.tick();
+
                     System.out.println("Thread " + Thread.currentThread().getId() + " received event with Lamport time "
                             + receivedTime);
+                    // Actualizamos el tiempo del reloj
                     clock.update(receivedTime);
                 }
             });
+            // Agregamos el hilo a la lista de hilos
             threads.add(thread);
+            // Iniciamos el hilo
             thread.start();
         }
         for (Thread thread : threads) {
             try {
+                // Esperamos a que todos los hilos terminen
                 thread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
